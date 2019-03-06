cd272ecd1c4d450802bf09f66622d45913d01ba1
Johny <mutant314@gmail.com>
2019-03-06 12:53:12 +0300

    Поправил метод getIntArr

--- Array.java
+++ Array.java
@@ -19,19 +19,24 @@ public class Array {
     public static int getIntArr(String[][] arr) throws MyArrayDataException {
         int[][] intArr = new int[4][4];
         int sum = 0;
-        for (int i = 0; i < arr.length; i++)
-            for (int j = 0; j < arr[i].length; j++) {
-                intArr[i][j] = Integer.parseInt(arr[i][j]);
-                if (intArr[i][j] instanceof String) throw new MyArrayDataException("Ошибка в ячейке " + "[" + (i + 1) + "][" + (j + 1) +"]", intArr[i][j]);
-                sum = sum + intArr[i][j];
-            }
+        try {
+            for (int i = 0; i < arr.length; i++)
+                for (int j = 0; j < arr[i].length; j++) {
+                    intArr[i][j] = Integer.parseInt(arr[i][j]);
+                    sum = sum + intArr[i][j];
+//                    if (intArr[i][j] instanceof String) throw new MyArrayDataException("Ошибка в ячейке " + "[" + (i + 1) + "][" + (j + 1) + "]", intArr[i][j]);
+                }
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
         return sum;
     }
 
     public static void main(String[] args) throws Exception {
         String[][] arr = {
                 {"1", "2", "4", "6"},
-                {"1", "3", "5", "7"},
+                {"1", "3", "5", "7o"},
                 {"5", "7", "9", "11"},
                 {"4", "6", "8", "10"},
         };
@@ -41,12 +46,12 @@ public class Array {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        try {
-            System.out.println(getIntArr(arr));
-        } catch (MyArrayDataException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
+//        try {
+            System.out.println("Сумма элементов массива равна "  + getIntArr(arr));
+//        } catch (MyArrayDataException e) {
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//        }
         System.out.println("Проверка");
     }
 }
